89dc9019834b36b7fad9f9e3c2eb8c2bcd54053e
Filippo2903 <filippo.gentili.06@gmail.com>
2022-12-28 20:11:01 +0100

    removed useless files

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	SokobanLevel testLevel(stoi(argv[1]), stoi(argv[2]), stoi(argv[3]));
+	SokobanLevel level(stoi(argv[1]), stoi(argv[2]), stoi(argv[3]));
 	
-	testLevel.Generate();
-	testLevel.Show();
+	level.Generate();
+	level.Show();
 
 	char input;
 	for (;;)
@@ -17,20 +17,20 @@ int main(int argc, char *argv[])
 		switch (input)
 		{
 		case 'w':
-			testLevel.PlayerMove(UP);
+			level.PlayerMove(UP);
 			break;
 		case 'a':
-			testLevel.PlayerMove(LEFT);
+			level.PlayerMove(LEFT);
 			break;
 		case 's':
-			testLevel.PlayerMove(DOWN);
+			level.PlayerMove(DOWN);
 			break;
 		case 'd':
-			testLevel.PlayerMove(RIGHT);
+			level.PlayerMove(RIGHT);
 			break;
 		case 'r':
 			system("cls");
-			testLevel.Generate();
+			level.Generate();
 			break;
 		case 'e':
 			return 0;
@@ -38,6 +38,6 @@ int main(int argc, char *argv[])
 			continue;
 		}
 		system("cls");
-		testLevel.Show();
+		level.Show();
 	}
 }
